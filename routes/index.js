76e478758115b9897650a4752152756b4d206114
Athif binu <binuathif@gmail.com>
2022-08-25 17:41:20 +0530

    admn and user add

--- routes/index.js
+++ routes/index.js
@@ -5,37 +5,32 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   let products=[
     {
-      name:"pedigree pro",
-      category:"Dog Food",
-      description:"Number One Qulity",
-      Image:"https://cdn.shopify.com/s/files/1/0086/0795/7054/products/edigreePROExpertNutritionLactatingPregnantMother_Pup_3-12Weeks_DryDogFoodFood_510x@2x.jpg?v=1636462181"
+      name:"royal Canin Puppy 1 kg",
+      category:"Dog food",
+      Discription:"Number One Quality",
+      Image:"https://images-eu.ssl-images-amazon.com/images/I/41237jNPwRS._SX300_SY300_QL70_FMwebp_.jpg"
     },
     {
-      name:"Royal canin",
-      category:"Dog Food",
-      description:"Number One Qulity",
-      Image:"https://images-eu.ssl-images-amazon.com/images/I/41gUl1uuqgL._SX300_SY300_QL70_FMwebp_.jpg"
-    
-      
-
-    },
-    {
-      name:"Drols Focus",
-      category:"Dog Food",
-      description:"Number One Qulity",
-      Image:"https://images-eu.ssl-images-amazon.com/images/I/41gUl1uuqgL._SX300_SY300_QL70_FMwebp_.jpg"
-    
-    },
-    {
-      name:"N & d",
-      category:"Dog Food",
-      description:"Number One Qulity",
+      name:"N & d Puppy 1 kg",
+      category:"Dog food",
+      Discription:"Number One Quality",
       Image:"https://m.media-amazon.com/images/I/71SWN1UlxjL._SY879_.jpg"
-    
-    }
-  
+    },
+    {
+      name:"pedigree pro",
+      category:"Dog food",
+      Discription:"Number One Quality",
+      Image:"https://m.media-amazon.com/images/I/41JzKCYG+SL._SX300_SY300_.jpg"
 
-  ]
+    },
+    {
+      name:"Drols focus",
+      category:"Dog food",
+      Discription:"Number One Quality",
+      Image:"https://m.media-amazon.com/images/I/61D9g9TtnQL._SX679_.jpg"
+    }
+  ]  
+   //this is passing page to intex hbs
   res.render('index', { products,admin:false });
 });
 
