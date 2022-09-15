ea445337112aefbaa4a4610a36e417aabe7c8cd4
Athif binu <binuathif@gmail.com>
2022-09-15 23:19:59 +0530

    error in cart

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -53,7 +53,7 @@ module.exports={
                 console.log(proExist)
                 if(proExist!=-1){
                      db.get().collection(collection.CART_COLLECTION)
-                     .updateOne({user:objectId(userId),'products.item':ObjectId(proId)},
+                     .updateOne({user:ObjectId(userId),'products.item':ObjectId(proId)},
                      {
                         $inc:{'products.$.quantity':1}
 
@@ -100,16 +100,13 @@ module.exports={
                 {
                     
                     $unwind: "$products"
-
                 },
                 {
                     $project:{
                         item:'$products.item',
-                        quantity:'$project.quantity'
+                        quantity:'$products.quantity'
                         
-                    }
-                    
-                    
+                    }      
                 },
                 {
                     $lookup:{
@@ -118,47 +115,68 @@ module.exports={
                         foreignField:'_id',
                         as:'product'
                     }
-                    
                 },
                 {
                     $project:{
                         item:1,quantity:1,product:{$arrayElemAt:['$product',0]}
                     }
                 }
-
               
             ]).toArray()
-        
             resolve(cartItems)
 
         })
     },
-
-
-            
-      changeProductQuantity:(details)=>{
-        details.count=parseInt(details.count)
-    
+    getCartCount:(userId)=>{
+        return new Promise(async(resolve,riject)=>{
+            let count=0
+            let cart=await db.get().collection(collection.CART_COLLECTION).findOne({user:ObjectId(userId)})
+            if(cart){
+                count=cart.products.length
+            }
+            resolve(count)
+        })
+    },
+    changeProductQuantity:(details)=>{
+      details.count=parseInt(details.count)
+      details.quantity=parent(details.quantity)
+        
         return new Promise((resolve,riject)=>{
+            if(details.count==1 && details.quantity==1){
             db.get().collection(collection.CART_COLLECTION)
-            .updateOne({_id:ObjectId(details.cart),'products.item':ObjectId(details.product)},
+            .updateOne({_id:ObjectId(details.cart)},
             {
-               $inc:{'products.$.quantity':details.count}
+               $pull:{products:{item:ObjectId(details.product)}}
 
             }
-            ).then(()=>{
-               resolve()
+            ).then((response)=>{
+               resolve({removeProduct:true})
             })
-        })
-      }
 
+       
+    }else{
+        db.get().collection(collection.CART_COLLECTION)
+           .updateOne({_id:ObjectId(details.cart),'products.item':ObjectId(details.product)},
+        {
+            $inc:{'products.$.quantity':details.count}
+        }
+    ).then((response)=>{
+        resolve(true)
 
+      })
+    }
 
-
-
+ })
+  }
 }
 
+    
+  
 
 
 
 
+
+
+
+        
